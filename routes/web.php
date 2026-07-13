34df8e439180c969488283f6a56bcad54274054a
Dogan Kaya <dogankayaa14@gmail.com>
2026-07-13 21:30:30 +0300

    Admin - User Sablonları Eklendi Admin Rotası Eklendi

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('admin', function () {
+    return view('admin.index');
+})->middleware('auth');
